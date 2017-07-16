7b7a03441c3759a9c3eb5e43ba7d4669f0da2740
Feynman Liang <feynman.liang@gmail.com>
2017-07-15 18:53:50 -0700

    Refactor into doctests

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+/// Example:
+///
+/// ```
+/// use concurrust::*;
+/// let ys = map(|x| x+1, vec![1,2,3]);
+/// assert_eq!(ys, vec![2,3,4])
+/// ```
 pub fn map<F,T,U>(f: F, xs: T ) -> Vec<U>
     where F: Fn(T::Item) -> U,
           T: IntoIterator {
@@ -8,6 +15,13 @@ pub fn map<F,T,U>(f: F, xs: T ) -> Vec<U>
     ys
 }
 
+/// Example:
+///
+/// ```
+/// use concurrust::*;
+/// let a = scan(|x, a| a+x, 0, vec![1,2,3]);
+/// assert_eq!(a, [0,1,3,6])
+/// ```
 pub fn reduce<F,T,A>(f: F, a0: A, xs: T) -> A
     where F: Fn(T::Item, A) -> A,
           T: IntoIterator {
@@ -18,6 +32,13 @@ pub fn reduce<F,T,A>(f: F, a0: A, xs: T) -> A
     a
 }
 
+/// Example:
+///
+/// ```
+/// use concurrust::*;
+/// let a = reduce(|x, a| a+x, 0, vec![1,2,3]);
+/// assert_eq!(a, 6)
+/// ```
 pub fn scan<F,T,A>(f: F, a0: A, xs: T) -> Vec<A>
     where F: Fn(T::Item, A) -> A,
           A: Copy,
@@ -31,45 +52,4 @@ pub fn scan<F,T,A>(f: F, a0: A, xs: T) -> Vec<A>
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn map_identity() {
-        let ys = map(|x| x, vec![1,2,3]);
-        assert_eq!(ys, vec![1,2,3])
-    }
-
-    #[test]
-    fn map_plus_one() {
-        let ys = map(|x| x+1, vec![1,2,3]);
-        assert_eq!(ys, vec![2,3,4])
-    }
-
-    #[test]
-    fn reduce_identity() {
-        let ys = reduce(|x, mut a| {
-            a.push(x);
-            a
-        }, vec![], vec![1,2,3]);
-        assert_eq!(ys, vec![1,2,3])
-    }
-
-    #[test]
-    fn reduce_sum() {
-        let a = reduce(|x, a| a+x, 0, vec![1,2,3]);
-        assert_eq!(a, 6)
-    }
-
-    #[test]
-    fn scan_identity() {
-        let a = scan(|x, a| x, 0, vec![1,2,3]);
-        assert_eq!(a, [0,1,2,3])
-    }
-
-    #[test]
-    fn scan_sum() {
-        let a = scan(|x, a| a+x, 0, vec![1,2,3]);
-        assert_eq!(a, [0,1,3,6])
-    }
-}
+mod tests {}
